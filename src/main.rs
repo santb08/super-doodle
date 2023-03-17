bddbd3eed2a8e352490aca8c224bac4774400e7d
JeffBosos <cristianxz999@gmail.com>
2023-03-17 17:47:14 -0500

    feat: added scale normalize and magnitude

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-use vector::Vector3;
-
-mod vector;
+mod positionin;
 
 fn main() {
-    let vec = Vector3 {
-        x: 10.0,
-        y: 20.0
-    };
-
-    vec.print();
-
-    let vec2 = Vector3 {
-        x: 5,
-        y: 6
-    };
-
-    // let result = vec.add(vec2);
-
-    let mag = vec.magnitude();
+    let mut vector = vec![1.0,2.0,3.0];
+    let orgi = vector.clone();
+    let scale = 2.0;
+    let mag = positionin::magnitude(vector[0],vector[1],vector[2]);
+    println!("{:?}",vector);
+    println!("The magnitude is {}",mag);
+    vector[0]=vector[0]/mag;
+    vector[1]=vector[1]/mag;
+    vector[2]=vector[2]/mag;
+    println!("normalized vector : {:?}",vector);
+    vector = orgi.clone();
+    vector[0]=vector[0]*scale;
+    vector[1]=vector[1]*scale;
+    vector[2]=vector[2]*scale;
+    println!("scale vector : {:?}",vector)
 }
