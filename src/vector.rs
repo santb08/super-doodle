301dc54f639b0b17118eab0ae8f042ffe7c3d2aa
Javier Ruiz Hoyos <jav.ruiz200@gmail.com>
2023-03-18 21:58:20 -0500

    feat: added, angle between vector, subtraction

--- src/vector.rs
+++ src/vector.rs
@@ -1,6 +1,6 @@
 pub struct Vector3 {
     pub x: f32,
-    pub y: f32
+    pub y: f32,
 }
 
 impl Vector3 {
@@ -8,12 +8,33 @@ impl Vector3 {
         println!("[Vector3] x: {} y: {}", self.x, self.y)
     }
 
-    pub fn add(self, vec: Vector3) -> Vector3 {
-        return self;
+    pub fn magnitude(&self) -> f32 {
+        (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
     }
 
-    pub fn magnitude(self) -> f32 {
-        let result = 10.0; // TODO: Calculate magnitude
-        result
+    pub fn add(&self, v2: &Vector3) -> Vector3 {
+        let x = self.x + v2.x;
+        let y = self.y + v2.y;
+        // Creamos un nuevo vector `resultado` vacío para almacenar la suma de los vectores
+        let result: Vector3 = Vector3 { x, y };
+        return result;
+    }
+
+    pub fn sub(&self, v2: &Vector3) -> Vector3 {
+        let x = self.x - v2.x;
+        let y = self.y - v2.y;
+        // Creamos un nuevo vector `resultado` vacío para almacenar la suma de los vectores
+        let result: Vector3 = Vector3 { x, y };
+        return result;
+    }
+
+    pub fn angle_between_vectors(&self, v2: &Vector3) -> Option<f32> {
+        let mag_product = self.magnitude() * v2.magnitude();
+        if mag_product == 0.0 {
+            return None;
+        }
+        let dot_product = self.x * v2.x + self.y * v2.y;
+        let cos_angle = dot_product / mag_product;
+        Some(cos_angle.acos())
     }
 }
